6e58a2496b0febcc79ca644f9fadc3b60e82d692
pblinkprosper <pblinkprosper@gmail.com>
2023-03-16 17:31:28 +0000

    Braces braces braces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-/* This code print a message on programming */
+/* This code prints out a message on programming */
 
 #include <stdio.h>
 
 int main(void)
 {
-	/* The puts function here acts like the printf to display
-	 * text written within the braces */
+	/*
+	 * The puts function here acts like the printf to display
+	 * text written within the braces
+	 */
 
 	puts("\"Programming is like building a multilingual puzzle");
 
