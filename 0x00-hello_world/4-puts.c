680266bd0af7f87e1957710c1381750ebeae4daf
pblinkprosper <pblinkprosper@gmail.com>
2023-03-16 17:45:26 +0000

    Double **

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,14 @@
 
 #include <stdio.h>
 
-/* The function main(void) serves as the staring point
+/**
+ * The function main(void) serves as the staring point
  * for the program execution
  */
 
 int main(void)
 {
-	/*
+	/**
 	 * The puts function here acts like the printf to display
 	 * text written within the braces
 	 */
