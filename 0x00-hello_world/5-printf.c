7ab7e64a0f368244611f0b8e02dde802c8c1a823
pblinkprosper <pblinkprosper@gmail.com>
2023-03-16 18:40:44 +0000

    My test commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+/* printf - printf tutorial */
+#include <stdio.h>
+/**
+ * main(void) - entry point
+ *
+ * Description - This is where the code
+ * execution begins
+ *
+ * return - If this is zero the code was successful
+ */
+int main(void)
+{
+	/**
+	 * printf - Prints whatever is given
+	 *
+	 * Description - Prints the phrase seen below as an output
+	 */
+	printf("with proper grammer, but the outcome is a piece of art");
+
+	/* return(0) - Always 0 (Success) */
+	return (0);
+}
