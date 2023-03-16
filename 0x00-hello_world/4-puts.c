3fffdcc7f4bd18adbf686d5a1b713e770de77c39
pblinkprosper <pblinkprosper@gmail.com>
2023-03-16 17:27:59 +0000

    The puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
+/* This code print a message on programming */
+
 #include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	/* The puts function here acts like the printf to display
+	 * text written within the braces */
+
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
