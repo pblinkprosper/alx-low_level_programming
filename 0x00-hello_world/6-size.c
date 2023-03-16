3d4ae5a0cc4943ca12497b5039ddb767a40562d9
pblinkprosper <pblinkprosper@gmail.com>
2023-03-16 21:44:21 +0000

    Long long int issue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -27,7 +27,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(charType));
 	printf("Size of an int: %d byte(s)\n", sizeof(integerType));
 	printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
-	printf("Size of a long long: %d byte(s)\n", sizeof(longlongType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 
 return (0);
