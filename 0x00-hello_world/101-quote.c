33921bf023e4123ed77e15be8915c34c157a541e
pblinkprosper <pblinkprosper@gmail.com>
2023-03-17 00:55:16 +0000

    Added the counters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@ int main(void)
 	/*
 	 * write - is a function that pints to the screen using system call
 	 */
-	puts("and that piece of art is useful\" - Dora Korper, 2015-10-19");
+	puts(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 60);
 
 	return (0);
 }
