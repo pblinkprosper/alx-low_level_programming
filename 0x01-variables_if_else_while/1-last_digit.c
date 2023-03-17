ed15a360d9bc082c4d243f9d0a789ba8debe90d6
pblinkprosper <pblinkprosper>
2023-03-17 19:04:35 +0000

    Minor whitespace fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l_digit = n % 10;
-	printf("Last digit of %d is %d", n, l_digit);
+	printf("Last digit of %d is %d ", n, l_digit);
 	if (l_digit > 5)
 	{
 		printf("and is greater than 5\n");
