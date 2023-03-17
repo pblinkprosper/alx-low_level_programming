b9d8bbd362447be3f58675472b329a89838ada0e
pblinkprosper <pblinkprosper>
2023-03-17 22:34:32 +0000

    Done and hopefully works

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print both lower and uppercase characters
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; ++abc)
+	{
+		putchar(abc);
+	}
+	for (abc = 'A'; abec <= 'Z'; ++abc)
+	{
+		putchar(abc)
+	}
+	putchar('\n');
+	return (0);
+}
