c3cd244eee6eefce36e6c235c1ff1d035f05e7fb
pblinkprosper <pblinkprosper>
2023-03-17 23:23:12 +0000

    Print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints from 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		printf("%d/n", i);
+	return (0);
+}
