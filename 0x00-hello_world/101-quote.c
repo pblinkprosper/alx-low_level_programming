b076dd141c0085afa9f596fae254dc91f1e8cf92
pblinkprosper <pblinkprosper@gmail.com>
2023-03-17 00:48:05 +0000

    The last one for the night

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/* This code prints with an error */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Code as alternative to printf
+ *
+ * Return: If return 0 (Success)
+ */
+int main(void)
+{
+	/*
+	 * write - is a function that pints to the screen using system call
+	 */
+	puts("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+
+	return (0);
+}
