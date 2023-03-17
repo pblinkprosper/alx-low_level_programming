a700416c2ac7935f7317640b143a6d260aec8c2a
pblinkprosper <pblinkprosper>
2023-03-17 23:04:37 +0000

    Omit some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints alphabets without q and e
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; ++abc)
+	{
+		if (abc != 'e' && abc != 'q')
+		{
+			putchar(abc);
+			putchar('\n');
+		}
+	}
+	return (0)
+}
