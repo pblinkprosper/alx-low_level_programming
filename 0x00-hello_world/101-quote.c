8c03d372d6879ebbd4ec32cbdbcfa278ff9b4d53
pblinkprosper <pblinkprosper@gmail.com>
2023-03-17 02:00:53 +0000

    Final final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@ int main(void)
 	/*
 	 * write - is a function that points to the screen using system call
 	 */
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
