133f6555be027ad3531f0cfd4de66b82396caa04
pblinkprosper <pblinkprosper>
2023-03-18 01:21:17 +0000

    he second attempt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; x < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 		if (i < 9)
@@ -20,6 +20,6 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	putchar(\n');
+	putchar('\n');
 	return (0);
 }
