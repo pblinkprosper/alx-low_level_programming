05749716d19ce19d02d8352411abe908be27f2e1
pblinkprosper <pblinkprosper>
2023-03-18 00:33:46 +0000

    Commit for hexes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all hex numbers in lowercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int hx;
+	char ha;
+
+	for (hx = '0'; hx <= '9'; ++hx)
+	{
+		putchar(hx);
+	}
+	for (ha = 'a'; <= 'f'; ++ha)
+	{
+		putchar(ha)
+	}
+	putchar('\n');
+	return (0);
+}
