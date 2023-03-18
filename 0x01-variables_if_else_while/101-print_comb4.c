a19a3713bdb7a0909e0f3fd36e94e422c40c5493
pblinkprosper <pblinkprosper>
2023-03-18 03:11:03 +0000

    Long code for 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: a code to print 3 unique number no repeats
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int e, f, g;
+
+	for (e = 0; e < 8; ++e)
+	{
+	for (f = e + 1; f < 9; ++f)
+	{
+	for (g = f + 1; g < 10; ++g)
+		{
+		if (e != f && e != g && f != g)
+		{
+		putchar(e + '0');
+		putchar(f + '0');
+		putchar(g + '0');
+		if (e + f + g < 24
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
