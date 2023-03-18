1152e84fc66a71df3399f2892cb805958a3eff28
pblinkprosper <pblinkprosper>
2023-03-18 17:29:57 +0000

    Hopefully all the hours of coding paid out

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations
+ * of two digits
+ *
+ * Return - Always 0 (success)
+ */
+int main(void)
+{
+    int i, j;
+
+	for (i = 0; i < 100; ++i)
+	{
+		for (j = i + 1; j < 100; ++j)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			{
+			if (i == 98 && j == 99)
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+    }
+    putchar('\n');
+    return (0);
+}
