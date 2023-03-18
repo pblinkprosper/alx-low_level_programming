24912d4fcb90ea12ae1b88c609e2908565bf06cb
pblinkprosper <pblinkprosper>
2023-03-18 01:57:55 +0000

    commit one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all combinations of two digits
+ *
+ * Result: always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; ++i)
+	{
+		for (j = i + 1; j < 10; ++j)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j ==9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n);
+	return (0);
+}
