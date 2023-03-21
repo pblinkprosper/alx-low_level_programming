8a7179a258b98c01b5ba7909519b6760f9ed84e2
pblinkprosper <pblinkprosper>
2023-03-21 13:53:35 +0000

    Write function update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,17 @@
  *
  * Return: always 0 success
  */
+int _putchar(char c)
+{
+	return write(STDOUT_FILENO, &c, 1);
+}
+
 int main(void)
 {
-	fwrite(1, "_putchar\n", 9);
+	char *str = "_putchar";
+	for (int i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
 	return (0);
 }
