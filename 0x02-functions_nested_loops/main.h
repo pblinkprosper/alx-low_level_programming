6a172e3c89844f4a6f999834ee0089fb27660352
pblinkprosper <pblinkprosper>
2023-03-21 16:12:48 +0000

    making the header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	print_alphabet();
 	return (0);
