4d0ecec2906fde7ad45c88c55f5c0290b28d4cf8
pblinkprosper <pblinkprosper>
2023-03-21 13:26:22 +0000

    Remove header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <main.h>
 /**
  * main - entry point
  *
@@ -8,7 +7,17 @@
 int main(void)
 {
 	char _, p, u, t, c, h, a, r;
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
 	putchar(_);
 	putchar(p);
 	putchar(u);
