815e2e5605558a4e145c3ebcb422cf5b29475749
pblinkprosper <pblinkprosper>
2023-03-21 15:56:41 +0000

    alphabets using prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 success
+ */
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
