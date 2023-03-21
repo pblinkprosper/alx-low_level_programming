57f74d6cc5b3125ba2c4ea7bed7b356b5bf4a32a
pblinkprosper <pblinkprosper>
2023-03-21 14:55:08 +0000

    Write function rewrite

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - entry point
  *
  * Return: always 0 success
  */
-int _putchar(char c)
-{
-	return write(STDOUT_FILENO, &c, 1);
-}
 
 int main(void)
 {
 	char *str = "_putchar";
-	for (int i = 0; i < strlen(str); i++)
-	{
-		_putchar(str[i]);
-	}
+	write (0, *str, 8);
 	return (0);
 }
