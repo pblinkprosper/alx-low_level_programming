510cab7953374f374fadf29281a5f2f553e64c66
pblinkprosper <pblinkprosper>
2023-03-22 13:57:58 +0000

    Adjustments to code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,16 @@
 #include "main.h"
 /**
  * _islower - checks if a character is lowercase
+ *
+ * Return: return 1 is success and 0 is false
  */
-int _islower(int)
+int _islower(int c1, c2, c3)
 {
+	/**
+	 * c1: first parameter to be tested
+	 * c2: second parameter to be tested
+	 * c3: third parameter to be tested
+	 */
 	int c1 = 'H';
 	int c2 = 'o';
 	int c3 = 108;
@@ -21,5 +28,6 @@ int _islower(int)
 		_putchar(c3 + '0');
 	}
 	_putchar('\n');
+	return (0);
 }
 
