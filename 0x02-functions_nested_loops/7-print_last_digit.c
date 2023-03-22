8d7af816b73b8d99329a9f9c5ea5773050ad8100
pblinkprosper <pblinkprosper>
2023-03-22 18:09:46 +0000

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a integer
+ * @l: parameter for printing last digit
+ * Return: always 0 (success)
+ */
+int print_last_digit(int l)
+{
+	_putchar((l / 10) + '0');
+}
