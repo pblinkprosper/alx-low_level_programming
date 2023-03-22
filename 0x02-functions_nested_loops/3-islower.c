285d058584c910360ad65f437e6c21b43c142a2a
pblinkprosper <pblinkprosper>
2023-03-22 13:47:28 +0000

    islower commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lowercase
+ */
+int _islower(int c)
+{
+	int c1 = 'H';
+	int c2 = 'o';
+	int c3 = '108';
+
+	if (_islower(c1))
+	{
+		_putchar(c1 + '0');
+	}
+	if (_islower(c2))
+	{
+		_putchar(c2 + '0';
+	}
+	if (_islower(c3))
+	{
+		_putchar(c3 + '0');
+	}
+	_putchar('\n');
+}
+
