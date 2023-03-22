40bfda51ba196f98163993033a2f0a0c055cffe1
pblinkprosper <pblinkprosper>
2023-03-22 20:13:24 +0000

    Changed return value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@ int print_last_digit(int l)
 {
 	int d;
 
-	d = (l / 10);
+	d = (l % 10);
 	if (d < 0)
 	{
 		d = -d;
 	}
-	_putchar((d % 10) + '0');
-	_putchar((d % 10) + '0');
-	return (0);
+	_putchar(d + '0');
+	return (d);
 }
