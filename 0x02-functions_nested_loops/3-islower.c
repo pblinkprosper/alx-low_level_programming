d6a8fa23414f40cb72fe3ba56141bde9e49ebaf2
pblinkprosper <pblinkprosper>
2023-03-22 14:06:24 +0000

    Corrected using if statement

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,28 +1,14 @@
 #include "main.h"
 /**
  * _islower - checks if a character is lowercase
- *
+ * @c: single integer input
  * Return: return 1 is success and 0 is false
  */
-int _islower(int)
+int _islower(int c)
 {
-	int c = 'H';
-	int c = 'o';
-	int c = 108;
-
-	if (_islower(c1))
-	{
-		_putchar(c1 + '0');
-	}
-	if (_islower(c2))
-	{
-		_putchar(c2 + '0');
-	}
-	if (_islower(c3))
-	{
-		_putchar(c3 + '0');
-	}
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
 
