7c99153cb337f72770d292ca43f62275f3a9596b
pblinkprosper <pblinkprosper>
2023-03-22 23:55:05 +0000

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i;
+	long long int fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+	for (i = 2; i < 50; i++)
+	{
+		fib[1] = fib[i - 1] + fib[1 -2];
+	}
+	for (i = 0; i < 50; i++)
+	{
+		printf("%lld, \n", fib[i]);
+	}
+	return (0);
+}
