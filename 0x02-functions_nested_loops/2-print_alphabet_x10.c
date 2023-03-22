24f72fce8dd2479063ba27b8ea210c209ded5078
pblinkprosper <pblinkprosper>
2023-03-22 12:19:39 +0000

    Alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: prints the alphabet 10 times
+ *
+ * Return: always 0 success
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	while (n < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		n++;
+		_putchar('\n');
+	}
+	return (0);
+}
