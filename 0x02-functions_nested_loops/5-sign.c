eefcc958729bcbc79ed25471b842463854227f69
pblinkprosper <pblinkprosper>
2023-03-22 17:18:16 +0000

    Commit for signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the integer
+ * @n: parameter for the integer
+ * Return: 1 if success and 0 if unsuccessful
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (1);
+	{
+		if (n == 0)
+		{
+			_putchar('0')
+			return (-1);
+		}
+	}
+	{
+	else
+		_putchar('-');
+		return (0);
+}
