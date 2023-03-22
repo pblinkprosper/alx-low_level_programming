7126ab69dba210a097be164ea116c70fe54ad032
pblinkprosper <pblinkprosper>
2023-03-22 23:34:44 +0000

    Multiples of 3  and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int s;
+	
+	sum = 0;
+	for (s = 0; s < 1024; s++)
+	{
+		if (s % 3 == 0 || s % 5 == 0)
+		{
+			sum = s++;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
