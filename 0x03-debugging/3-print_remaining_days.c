7fbd0a07e2c4a6d289fd71b51f145b8b86827ec9
pblinkprosper <pblinkprosper>
2023-03-23 22:46:42 +0000

    Print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,34 +2,36 @@
 #include "main.h"
 
 /**
- * print_remaining_days - prints days left in a year including leap years
- * @month: parameter for month
- * @day: parameter for day
- * @year: parameter for year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) && (year % 100 == 0 || year % 400 != 0))
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-	if (month >= 3 && day >= 60)
+	if (month >= 2 && day >= 60)
 	{
-	day++;
+		day++;
 	}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-	if (month == 2 && day >= 60)
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, y);
+
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 366 - day);
 	}
 	else
+	{
+		if (month == 2 && day == 60)
+	{
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	}
+		else
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 	}
-	}
+    }
 }
