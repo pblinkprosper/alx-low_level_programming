4b88979fe095f7a165f3c632ac7f15aa6b693a71
pblinkprosper <pblinkprosper>
2023-03-23 17:56:52 +0000

    First commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - tests ifunction that prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i);
+{
+	int i;
+
+	i = 0;
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# Readme for the the Debugging task
+- All functions on this task will be highlighted here
