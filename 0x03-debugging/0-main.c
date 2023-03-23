678e714f726f15de5ee5bdd60f3e80f8c073318a
pblinkprosper <pblinkprosper>
2023-03-23 19:24:14 +0000

    Git commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests ifunction that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - entry point
+ * @i: parameter that holds integer to be tested
+ *
  * Return: always 0 success
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("%d ", n);
-	if (n > 0)
+	i = rand() - RAND_MAX / 2;
+
+	printf("%d ", i);
+	if (i > 0)
 	{
 		printf("is positive\n");
 	}
-	if (n == 0)
+	if (i == 0)
 	{
 		printf("is zero\n");
 	}
-	if (n < 0)
+	if (i < 0)
 	{
 		printf("is negative\n");
 	}
-	return (0);
 }
