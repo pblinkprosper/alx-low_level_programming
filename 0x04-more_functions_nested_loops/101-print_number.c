a804bd9cb36f7f7b435762d026864bb013f0b99d
pblinkprosper <pblinkprosper>
2023-03-24 00:54:57 +0000

    Final commit on project for printing integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,26 @@
  */
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int n2, n1 = n;
+	int div = 1;
 
-	a = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		a = -n;
+		_putchar('-');
+		n1 = n * -1;
 	}
-	if (a / 10)
-		print_number(a / 10);
-		_putchar((a % 10) + '0');
-}
+	n2 = n1;
+	while (n2 > 9)
+	{
+		div *= 10;
+		n2 = n2 / 10;
+	}
+	while (div >= 1)
+	{
+		n2 = n1 % div;
+		n1 /= div;
+		_putchar(n1 + '0');
+		n1 = n2;
+		div /= 10;
+	}
+}}
