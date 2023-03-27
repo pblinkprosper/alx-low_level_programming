2c7c40981dfbdec7222db3ed0e59401f0d703b44
pblinkprosper <pblinkprosper>
2023-03-27 13:57:47 +0000

    Swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @c: parameter to swap first integer
+ * @d: parameter to swap second integer
+ *
+ * Return: nothing
+ */
+void swap_int(int *c, int *d)
+{
+	*c = 42;
+	*d = 98;
+}
