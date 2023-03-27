17ba1be72a1a95d95518240617fb44b3de173e7e
pblinkprosper <pblinkprosper>
2023-03-27 13:23:22 +0000

    Reset parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets the integer to 98
+ * @p: a pointer the integer we want to set to 98
+ *
+ * Return: 0
+ */
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# This README details the contents of this project on pointers, arrays and strings
