6cb6382d1667084a884244e6d73561666e0c85fd
pblinkprosper <pblinkprosper>
2023-03-29 23:20:05 +0000

    simplify my code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i]; i++)
 	{
 		;
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	for (j = 0; j < n && src[j]; j++)
 	{
 		dest[i] = src[j];
 		i++;
