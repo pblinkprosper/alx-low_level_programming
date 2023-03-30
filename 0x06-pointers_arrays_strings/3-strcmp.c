dbcb54df4af8e0bf0e3a21892243b848af9af373
pblinkprosper <pblinkprosper>
2023-03-30 01:04:31 +0000

    Corrected while statement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int r;
+	int n;
 
 	r = 0;
 	while (s1[r] == s2[r] && (s1[r] != '\0' || s2[r] != '\0'))
+	{
 		r++;
-	return (*s1 - *s2);
+	}
+	n = s1[r] - s2[r];
+	return (n);
 }
