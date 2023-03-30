9785ad557c211d7e1f228bcc39bfef5f0f5a18b2
pblinkprosper <pblinkprosper>
2023-03-30 12:19:53 +0000

    Convert to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - this function converts all lowercase letter to uppercase
+ *
+ * Return: Always 0
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] >= 'a') && (str[i] <= 'z'))
+		{
+			str[i] -= 32;
+		}
+	}
+	return(str);
+}
