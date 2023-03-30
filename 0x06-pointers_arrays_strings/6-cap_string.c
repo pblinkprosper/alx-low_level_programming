ecf1be461e553c1da42792f6334f438f3f136bfc
pblinkprosper <pblinkprosper>
2023-03-30 20:16:16 +0000

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalises all words of a string
+ * @str: parameter for storing the string to be used
+ *
+ * Return: returns char
+ */
+char *cap_string(char *str)
+{
+	int i, j, k;
+
+	char syms[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	j = 32;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+		if (str[i] >= 'a' && str [i] <= 'z')
+		{
+			str[i] = str[i] - j;
+		}
+		j = 0;
+		for (k = 0; syms[k]; k++)
+		{
+			if (syms[k] == str[i])
+			{
+				j = 32;
+				break;
+			}
+		}
+	}
+	return (str);
+}
