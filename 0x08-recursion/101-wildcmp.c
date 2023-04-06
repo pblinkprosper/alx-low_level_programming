684ee4d01fd55feb16419499e883df6257e1e533
pblinkprosper <pblinkprosper>
2023-04-06 21:31:28 +0000

    Compare two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * wildcmp - this function compares two strings
+ * @s1: the first string
+ * @s2: the second string which can contain * as a special character
+ *
+ * Return: returns 1 if identical or 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
