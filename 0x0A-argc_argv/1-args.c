176c493d7dafc3c9760ae5f95dd6bd8c4f6dc157
pblinkprosper <pblinkprosper>
2023-04-08 15:55:11 +0000

    Check number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: the number of arguments passed into the program
+ * @argv: an array of strings passed to the program
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	for (argc = 0; argv[argc]; argc++)
+		printf("%d\n", argc);
+	return (0);
+}
