74b665c9fa4e0ef7754b2715046cea8d92491404
pblinkprosper <pblinkprosper>
2023-04-08 18:10:05 +0000

    Multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: the number of arguments passed into the program
+ * @argv: an array of strings passed to the program
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
