7a0edac4c904fb52445da2bc20779f156a6ffe5d
pblinkprosper <pblinkprosper>
2023-04-10 12:45:35 +0000

    Static libraries correction

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,10 @@
-#include <unistd.h> 
+#include <unistd.h>
+
 /**
- * _putchar - write the character a to stout
- * @a: The character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 success.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
