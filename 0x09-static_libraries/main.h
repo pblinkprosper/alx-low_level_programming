1d5baaead2d94d4d52001ae960c5cef3f9eb31c3
pblinkprosper <pblinkprosper>
2023-04-10 13:02:01 +0000

    Header file test

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 #include <unistd.h>
 int _putchar(char c);
 int _islower(int c);
@@ -21,4 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
