e83480d3c4da0eca693913005bc8592c6a6a763a
pblinkprosper <pblinkprosper>
2023-04-12 00:00:15 +0000

    Free and check with valgrind

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - this function frees the 2 dimensional grid previously created
+ * @grid: parameter pointer to pointer for grid
+ * @height: parameter for grid height
+ *
+ * Return: function returns nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int k;
+	for (k = 0; k < height; k++)
+		free(grid[k]);
+	free (grid);
+}
