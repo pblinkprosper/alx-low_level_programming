e0ef5f948757ae7db42aef396d6412457174b3bc
pblinkprosper <pblinkprosper>
2023-04-13 02:18:58 +0000

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,121 +1,77 @@
-#include "main.h"
 #include <stdlib.h>
-/**
- * strncat_cpy - function that concatenates two strings using n bytes from src
- * @dest: parameter for destination string
- * @src: parameter for source string
- * @i: start of string to copy
- * @n: number of bytes
- *
- * Return: returns value of dest string
- */
-char strncat_cpy(char *dest, char *src, int i, int n)
-{
-	int j;
+#include "main.h"
 
-	for (j = 0; src[i] != ' ' && i < n; j++)
-	{
-		dest[j] = src[i];
-		i++;
-	}
-	return (i);
-}
 /**
- * mallocalloc - this function allocates memory for the output
- * array and sets NULL at string end
- * @nstr: the new string to be allocated memory
- * @str: the main string
- * @n: length of the string
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: 0
+ * Return: number of words
  */
-void mallocalloc(char **nstr, char *str, int n)
+int count_word(char *s)
 {
-	int i, j, wn;
+	int flag, c, w;
 
-	i = 0;
-	j = 0;
-	wn = 1;
-	while (i < n)
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (str[i] != ' ')
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			while (str[i] != ' ' && i < n)
-			{
-				i++, wn++;
-			}
-		nstr[j] = malloc(sizeof(char) * wn);
-		nstr[j][wn] = '\0';
-		j++, wn = 1;
+			flag = 1;
+			w++;
 		}
-	i++;
 	}
-}
-/**
- * word_check - this checks the number of words in a string
- * @str: the string to be checked
- * @n: the lenght of the string
- *
- * Return: 0 if fail and words if succeed
- */
-int word_check(char *str, int n)
-{
-	int i, words;
 
-	i = 0;
-	words = 0;
-	while (i < n)
-	{
-		if (str[i] != ' ')
-		{
-			while (str[i] != ' ' && i < n)
-			{
-				i++;
-				words++;
-			}
-		}
-	i++;
-	}
-	if (words == 0)
-	return (0);
-	return (words);
+	return (w);
 }
 /**
- * strtow - this function splits a string into words
- * @str: the string to be split
+ * **strtow - splits a string into words
+ * @str: string to split
  *
- * Return: pointer to new string
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	char **nstr;
-	int i, j, n, words;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	i = 0;
-	j = 0;
-	n = 0;
-	if (str == NULL || str[0] == '\0')
-	return (NULL);
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
-	while (*(str + n) != '\0')
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
-		n++;
-	}
-	words = word_check(str, n);
-	if (!words)
-	return (NULL);
-
-	nstr = malloc(sizeof(char *) * (words + 1));
-	mallocalloc(nstr, str, n);
-	while (i < n)
-	{
-		if (str[i] != ' ')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			i = strncat_cpy(nstr[j], str, i, n);
-			j++, i--;
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
 		}
-	i++;
+		else if (c++ == 0)
+			start = i;
 	}
-	nstr[words + 1] = NULL;
-	return (nstr);
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
---
+++ 0x0B-malloc_free/102-strtow.c
@@ -0,0 +1,121 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strncat_cpy - function that concatenates two strings using n bytes from src
+ * @dest: parameter for destination string
+ * @src: parameter for source string
+ * @i: start of string to copy
+ * @n: number of bytes
+ *
+ * Return: returns value of dest string
+ */
+char strncat_cpy(char *dest, char *src, int i, int n)
+{
+	int j;
+
+	for (j = 0; src[i] != ' ' && i < n; j++)
+	{
+		dest[j] = src[i];
+		i++;
+	}
+	return (i);
+}
+/**
+ * mallocalloc - this function allocates memory for the output
+ * array and sets NULL at string end
+ * @nstr: the new string to be allocated memory
+ * @str: the main string
+ * @n: length of the string
+ *
+ * Return: 0
+ */
+void mallocalloc(char **nstr, char *str, int n)
+{
+	int i, j, wn;
+
+	i = 0;
+	j = 0;
+	wn = 1;
+	while (i < n)
+	{
+		if (str[i] != ' ')
+		{
+			while (str[i] != ' ' && i < n)
+			{
+				i++, wn++;
+			}
+		nstr[j] = malloc(sizeof(char) * wn);
+		nstr[j][wn] = '\0';
+		j++, wn = 1;
+		}
+	i++;
+	}
+}
+/**
+ * word_check - this checks the number of words in a string
+ * @str: the string to be checked
+ * @n: the lenght of the string
+ *
+ * Return: 0 if fail and words if succeed
+ */
+int word_check(char *str, int n)
+{
+	int i, words;
+
+	i = 0;
+	words = 0;
+	while (i < n)
+	{
+		if (str[i] != ' ')
+		{
+			while (str[i] != ' ' && i < n)
+			{
+				i++;
+				words++;
+			}
+		}
+	i++;
+	}
+	if (words == 0)
+	return (0);
+	return (words);
+}
+/**
+ * strtow - this function splits a string into words
+ * @str: the string to be split
+ *
+ * Return: pointer to new string
+ */
+char **strtow(char *str)
+{
+	char **nstr;
+	int i, j, n, words;
+
+	i = 0;
+	j = 0;
+	n = 0;
+	if (str == NULL || str[0] == '\0')
+	return (NULL);
+
+	while (*(str + n) != '\0')
+	{
+		n++;
+	}
+	words = word_check(str, n);
+	if (!words)
+	return (NULL);
+
+	nstr = malloc(sizeof(char *) * (words + 1));
+	mallocalloc(nstr, str, n);
+	while (i < n)
+	{
+		if (str[i] != ' ')
+		{
+			i = strncat_cpy(nstr[j], str, i, n);
+			j++, i--;
+		}
+	i++;
+	}
+	nstr[words + 1] = NULL;
+	return (nstr);
+}
