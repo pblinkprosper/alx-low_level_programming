ff059e85fc2ff072c4c7fb9b615aaa3ab211ce13
pblinkprosper <pblinkprosper>
2023-04-17 23:10:07 +0000

    Defining a typedef on struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
