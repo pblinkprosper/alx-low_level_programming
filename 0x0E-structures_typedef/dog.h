7823ce1055e65bc2a26c4b49e0a8947cc6cec660
pblinkprosper <pblinkprosper>
2023-04-17 17:53:28 +0000

    Structure for Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function name
+ * @name: parameter for char data type
+ * @age: parameter for float data type
+ * @owner: parameter for char data type
+ *
+ * Description: this function takes 3 members and prints to screen when called
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
