d548f81a5a2f4dbd7d4473998de68f4d3cee8001
pblinkprosper <pblinkprosper>
2023-04-17 22:57:40 +0000

    Print the struct correction 03

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,19 +9,22 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		return;
-	}
-	else if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
-	else if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
+		if (d == NULL)
+		{
+			return;
+		}
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
+	}
 }
