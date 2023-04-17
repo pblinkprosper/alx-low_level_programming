c5f1f235ac8acc4c781ae7b1d549905cef4f241f
pblinkprosper <pblinkprosper>
2023-04-17 23:58:35 +0000

    Free all memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - this function frees memory used by the dog function
+ * @d: pointer holding string for typedef struct
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
