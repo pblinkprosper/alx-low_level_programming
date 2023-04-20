355f99bb0119a25cbaaefb1b97d3c417924f220d
pblinkprosper <pblinkprosper>
2023-04-20 08:29:39 +0000

    Creating a function pointer

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# README document for the project function pointers
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCITON_POINTER_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
