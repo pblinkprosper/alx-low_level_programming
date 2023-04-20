bde6dce2d630abfb44480d48a126f6ec0279315f
pblinkprosper <pblinkprosper>
2023-04-20 15:06:43 +0000

    Search for and return integer plus betty compliance

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,8 +5,8 @@
  * @size: the size of the array
  * @cmp: function given to initiate the search
  *
- * Return: returns the index of the first element for which the given function does
- * not return 0
+ * Return: returns the index of the first element for which the given function
+ * does not return 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return(i);
+			return (i);
 	}
 	return (-1);
 }
