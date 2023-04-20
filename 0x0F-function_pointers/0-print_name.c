fdccfd4dd422e9acbb4e6a6095b49a1bfb2f56ad
pblinkprosper <pblinkprosper>
2023-04-20 08:33:36 +0000

    Creating a function pointer and betty compliance

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,11 @@
 #include "function_pointers.h"
+/**
+ * print_name - this function prints a name
+ * @name: pointer to the name string
+ * @f: function pointer
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
