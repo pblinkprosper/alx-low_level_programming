09e2bbc145c18dfe773abe74cec1e52c4ac5c9b7
pblinkprosper <pblinkprosper>
2023-04-20 21:53:53 +0000

    Add all files

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main -  program that prints the opcodes of its own main function.
+*@argc: number of arguments passed to the program
+*@argv: array of arguments
+*
+*Return: on success, 1 or 2 in case of failure
+*/
+/**
+int main(int argc, char *argv[])
+{
+
+int index, bytes;
+int (*address)(int, char **) = main;
+unsigned char opcode;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index <  bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
+return (0);
+}
+*/
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nbytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
