a4b73294277039b87373b12d57ffa48458c4bb14
pblinkprosper <pblinkprosper>
2023-05-01 20:49:52 +0000

    Task 9 files push

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - this function inserts a new node at a given index
+ * @head: this is the first node
+ * @idx: this is the index of the node
+ * @n: this is the counter
+ *
+ * Return: returns the address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	
+}
