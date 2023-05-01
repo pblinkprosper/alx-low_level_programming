35021a92e57e2f37b899994520ea8501753014ed
pblinkprosper <pblinkprosper>
2023-05-01 18:27:10 +0000

    Free memory2 correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_ti *temp;
 
-	if (head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
-		while (*head)
-		{
-			temp = *head;
-			free(temp);
-			(*head) = (*head)->next;
-		}
-	*head = NULL;
+		temp = (*head);
+		free(temp);
+		(*head) = (*head)->next;
 	}
+	(*head) = NULL;
 }
