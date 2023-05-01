8cdff6da3842ef82cd31767a5e78c1a78916ae84
pblinkprosper <pblinkprosper>
2023-05-01 18:24:08 +0000

    Free memory2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - this function frees memory held by the list
+ * @head: pointer to a pointer to the first node in the list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head != NULL)
+	{
+		while (*head)
+		{
+			temp = *head;
+			free(temp);
+			(*head) = (*head)->next;
+		}
+	*head = NULL;
+	}
+}
