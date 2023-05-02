19da3dfbcb30ddee34dbfa8aa8c5d10411a4ea4c
pblinkprosper <pblinkprosper>
2023-05-02 23:08:17 +0000

    Print linked list with loop, correction 11

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -21,8 +21,10 @@ size_t print_listint_safe(const listint_t *head)
 		if (temp <= head)
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			break;
 		}
 	}
+	if (!temp)
+		exit(98);
 	return (count);
 }
