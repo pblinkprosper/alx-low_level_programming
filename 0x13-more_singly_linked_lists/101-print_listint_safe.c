62d35ddc1c74ebc80224d706a508e0e08b7b9871
pblinkprosper <pblinkprosper>
2023-05-02 22:51:19 +0000

    Added exit statement to code updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -21,8 +21,10 @@ size_t print_listint_safe(const listint_t *head)
 		if (temp <= head)
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			break;
 		}
 	}
+	if (head)
+		exit(98);
 	return (count);
 }
