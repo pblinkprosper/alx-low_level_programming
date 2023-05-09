8a990ae923c1aa63953a50a83a973cd1edd406eb
pblinkprosper <pblinkprosper>
2023-05-09 00:05:17 +0000

    Create main function to copy file to another file correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	if (file == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	file2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
