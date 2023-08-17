d035646f9c65776d9001d07ab803c4a3ccd22dce
Prosper <pblinkprosper01@gmail.com>
2023-08-17 23:38:14 +0000

    Add at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function returns nth node
+ *
+ * @head: pointer to head node
+ *
+ * @index: index to place node
+ *
+ * Return: the list or NULL if it doesn't exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head)
+	{
+		if (index == i)
+		{
+			return (head);
+		}
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
