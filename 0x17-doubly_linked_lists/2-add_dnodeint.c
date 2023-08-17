baa487fe6fe5c76900598cbb7f7e6dd251507514
Prosper <pblinkprosper01@gmail.com>
2023-08-17 22:37:10 +0000

    Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - this function adds a node at the begining of the list
+ * @head: head node of the list
+ * n: the value
+ *
+ * Return: the address of new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *n_node;
+
+	n_node = malloc(sizeof(dlistint_t));
+	if (!n_node)
+		return (NULL);
+	n_node->n = n;
+	n_node->next = *head;
+	n_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = n_node;
+	*head = n_node;
+
+	return (n_node);
+}
